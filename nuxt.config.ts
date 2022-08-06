8c7ee0f025273a9aa5b851d3b1e575a4e7e6c51a
Felix Scholze <felix@webshaped.de>
2022-08-06 13:17:01 +0200

    chore: add env vars + enable static mode

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,27 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+  typescript: {
+    shim: false
+  },
 
+  // ENV variables -> https://v3.nuxtjs.org/guide/features/runtime-config
+  runtimeConfig: {
+    // The private keys which are only available within server-side
+
+    // Keys within public, will be also exposed to the client-side
+    public: {
+      apiBase: '',
+    }
+  },
+
+  // Enables static mode
+  ssr: false,
+
+  // Prerender Routes -> https://v3.nuxtjs.org/guide/deploy/static-hosting/#advanced
+  nitro: {
+    prerender: {
+      routes: []
+    }
+  },
 })
